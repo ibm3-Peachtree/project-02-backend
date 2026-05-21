811ae0ef23dcc2996e11a0113bdaf045310df0a9
Seunghee Choi <seungjj1104@gmail.com>
2026-05-21 11:53:08 +0900

    test: 오류 해결 후 시도

---
+++ src/main/java/com/ruttu/project_02_backend/dto/routine/SpeedDto.java
@@ -0,0 +1,12 @@
+package com.ruttu.project_02_backend.dto.routine;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SpeedDto {
+    private double speed;
+}
