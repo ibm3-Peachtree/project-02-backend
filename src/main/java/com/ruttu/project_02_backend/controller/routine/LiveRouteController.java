4ac715deda1236effc0b5bb64e4642fdb384dc97
Seunghee Choi <seungjj1104@gmail.com>
2026-05-21 11:37:17 +0900

    feat: getRouteProgress 추가

--- src/main/java/com/ruttu/project_02_backend/controller/routine/LiveRouteController.java
+++ src/main/java/com/ruttu/project_02_backend/controller/routine/LiveRouteController.java
@@ -2,6 +2,11 @@ package com.ruttu.project_02_backend.controller.routine;
 
 import com.ruttu.project_02_backend.dto.routine.CurrentLocationDto;
 import com.ruttu.project_02_backend.service.routine.LiveRouteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +22,29 @@ public class LiveRouteController {
 
     private final LiveRouteService liveRouteService;
 
+    @Operation(
+            summary = "현재 단계 조회",
+            description = "도보중/대기중/탑승중 표시"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "단계 조회 성공",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = CurrentLocationDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "단계 조회 실패",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Void.class)
+                    )
+            )
+    }
+    )
     @GetMapping("/status")
     public ResponseEntity<CurrentLocationDto> getRouteProgress(){
 
