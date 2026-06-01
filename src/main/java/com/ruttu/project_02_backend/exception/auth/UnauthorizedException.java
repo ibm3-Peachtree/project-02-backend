a9e1fb953d6fa48ffba92c26c46d362c8b1393a0
Seunghee Choi <seungjj1104@gmail.com>
2026-06-02 07:28:33 +0900

    feat: exception에 http status 추가

--- src/main/java/com/ruttu/project_02_backend/exception/auth/UnauthorizedException.java
+++ src/main/java/com/ruttu/project_02_backend/exception/auth/UnauthorizedException.java
@@ -1,5 +1,11 @@
 package com.ruttu.project_02_backend.exception.auth;
 
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@NoArgsConstructor
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class UnauthorizedException extends RuntimeException {
     public UnauthorizedException(String message) {
         super(message);
--- src/main/java/com/ruttu/project_02_backend/exception/routine/RecommendationNotFoundException.java
+++ src/main/java/com/ruttu/project_02_backend/exception/routine/RecommendationNotFoundException.java
@@ -1,8 +1,11 @@
 package com.ruttu.project_02_backend.exception.routine;
 
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @NoArgsConstructor
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class RecommendationNotFoundException extends RuntimeException{
     public RecommendationNotFoundException(String msg){
         super(msg);
