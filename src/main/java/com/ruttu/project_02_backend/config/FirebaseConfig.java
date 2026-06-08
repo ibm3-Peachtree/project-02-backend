87ce7bbf90d8585bcfaea2fc98ffb3fb35836b6c
Seunghee Choi <seungjj1104@gmail.com>
2026-06-08 22:24:03 +0900

    feat: firebase-admin.json 불러오는 방식 수정

--- src/main/java/com/ruttu/project_02_backend/config/FirebaseConfig.java
+++ src/main/java/com/ruttu/project_02_backend/config/FirebaseConfig.java
@@ -4,32 +4,39 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
+@RequiredArgsConstructor
 @Configuration
 public class FirebaseConfig {
+
+    @Value("${firebase.config}")
+    private String firebaseConfig;
 
     @PostConstruct
     public void init() throws IOException {
 
         InputStream inputStream =
-                new ClassPathResource(
-                        "firebase-admin.json"
-                ).getInputStream();
+                new ByteArrayInputStream(
+                        firebaseConfig.getBytes(StandardCharsets.UTF_8)
+                );
 
         FirebaseOptions options =
                 FirebaseOptions.builder()
                         .setCredentials(
-                                GoogleCredentials.fromStream(
-                                        inputStream
-                                )
+                                GoogleCredentials.fromStream(inputStream)
                         )
                         .build();
 
-        FirebaseApp.initializeApp(options);
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
     }
 }
